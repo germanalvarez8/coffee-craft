c6489a8740b2a6b2b5edc436a19e113cf9fe63dc
germanalvarez8 <ger.22.alvarez@gmail.com>
2024-08-07 21:53:40 -0300

    fixeado

--- boostrap/scripts/data.js
+++ boostrap/scripts/data.js
@@ -3,47 +3,43 @@ let products = [
         id: 1,
         title: 'Bolivia Honey',
         description: 'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.',
-        images: ['/boostrap/images/bolivia-honey.jpg']
+        images: '/boostrap/images/bolivia-honey.jpg'
     },
     {
         id: 2,
         title: 'Colombia Geisha',
         description: 'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.',
-        images: ['/boostrap/images/bolivia.jpg']
+        images: '/boostrap/images/bolivia.jpg'
     },
     {
         id: 3,
         title: 'Colombia Dragon',
         description: 'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.',
-        images: ['/boostrap/images/colombia-dragon.jpg']
+        images: '/boostrap/images/colombia-dragon.jpg'
     },
     {
         id: 4,
         title: 'Colombia Natural',
         description: 'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.',
-        images: ['/boostrap/images/colombia-natural.jpg']
+        images: '/boostrap/images/colombia-natural.jpg'
     },
     {
         id: 5,
         title: 'Colombia',
         description: 'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.',
-        images: ['/boostrap/images/colombia.jpg']
+        images: '/boostrap/images/colombia.jpg'
     },
     {
         id: 6,
         title: 'Brasil Dark',
         description: 'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.',
-        images: ['/boostrap/images/brasil-dark.jpg']
+        images: '/boostrap/images/brasil-dark.jpg'
     },
     {
         id: 7,
         title: 'Brasil',
         description: 'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.',
-        images: [
-            '/boostrap/images/brasil.jpg',
-            '/boostrap/images/brasil-filtro.jpg',
-            '/boostrap/images/brasil-moka.jpg',
-        ]
+        images: '/boostrap/images/brasil.jpg'
     }
 ]
 
