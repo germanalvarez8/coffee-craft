a7ed0e54da10b1048ee290d42b23b5d8031fb280
ValenCardozo <valencardozo7@gmail.com>
2024-08-18 22:00:58 -0300

    feat(styles): add a lot a fixes

--- boostrap/scripts/data.js
+++ boostrap/scripts/data.js
@@ -4,49 +4,56 @@ let products = [
         title: 'Bolivia Honey',
         description: 'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.',
         images: '/images/bolivia-honey.jpg',
-        flag_image: '../images/bolivia-flag_140x140.jpeg'
+        flag_image: '../images/bolivia-flag_140x140.jpeg',
+        price: 10.00
     },
     {
         id: 2,
         title: 'Colombia Geisha',
         description: 'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.',
         images: '/images/bolivia.jpg',
-        flag_image: '../images/colombia-flag_140x140.jpeg'
+        flag_image: '../images/colombia-flag_140x140.jpeg',
+        price: 20.00
     },
     {
         id: 3,
         title: 'Colombia Dragon',
         description: 'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.',
         images: '/images/colombia-dragon.jpg',
-        flag_image: '../images/colombia-flag_140x140.jpeg'
+        flag_image: '../images/colombia-flag_140x140.jpeg',
+        price: 15.00
     },
     {
         id: 4,
         title: 'Colombia Natural',
         description: 'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.',
         images: '/images/colombia-natural.jpg',
-        flag_image: '../images/colombia-flag_140x140.jpeg'
+        flag_image: '../images/colombia-flag_140x140.jpeg',
+        price: 12.15
     },
     {
         id: 5,
         title: 'Colombia',
         description: 'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.',
         images: '/images/colombia.jpg',
-        flag_image: '../images/colombia-flag_140x140.jpeg'
+        flag_image: '../images/colombia-flag_140x140.jpeg',
+        price: 23.00
     },
     {
         id: 6,
         title: 'Brasil Dark',
         description: 'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.',
         images: '/images/brasil-dark.jpg',
-        flag_image: '../images/brasil-flag_140x140.jpeg'
+        flag_image: '../images/brasil-flag_140x140.jpeg',
+        price: 30.00
     },
     {
         id: 7,
         title: 'Brasil',
         description: 'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.',
         images: '/images/brasil.jpg',
-        flag_image: '../images/brasil-flag_140x140.jpeg'
+        flag_image: '../images/brasil-flag_140x140.jpeg',
+        price: 27.50
     }
 ]
 
@@ -61,12 +68,12 @@ function addToCart(element) {
             }
         });
     } else {
+        console.log(element);
         shopCart.push({
             id: element.dataset.id,
             name: element.dataset.name,
-            description: 'description',
             quantity: 1,
-            price: 100
+            price: element.dataset.price
         });
     }
 
